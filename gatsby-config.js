15b7f53df82e4bd6367638a21a8e1a1b4559a7a6
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-15 16:11:36 +0530

    📦 NEW: Add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@majhirockzz",
+    person: { name: "Sumesh Majhi", age: 25 },
+    simpleData: ["item1", "item2"],
+    complexData: [
+      { name: "Sumesh", age: 25 },
+      { name: "Susan", age: 21 },
+      { name: "John", age: 32 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
