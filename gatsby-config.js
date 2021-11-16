bc375fa5fadbe56e3a098b87d91c2162a4c7b564
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-16 08:22:14 +0530

    📦 NEW: Connect Gatsby - Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
   siteMetadata: {
     title: "Simply Recipes",
     description: "Nice and clean recipes site",
@@ -28,6 +25,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `asewku8uqann`,
+        accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
   ],
