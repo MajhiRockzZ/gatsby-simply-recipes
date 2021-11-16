6e09fad2b2694fa564e9da9837499e17746faa7d
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-16 18:39:01 +0530

    🐛 FIX: NavBar Is Not Found

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import "normalize.css"
 import React from "react"
 import "../assets/css/main.css"
 import Footer from "./Footer"
-import Navbar from "./NavBar"
+import Navbar from "./Navbar"
 
 function Layout(props) {
   return (
