a6b6b9fd0e2eada666683d05e111533a5ffdc339
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-17 08:15:48 +0530

    📦 NEW: Add Testing Page

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Gallery from "../examples/Gallery"
+import SEO from "../components/SEO"
 
 const TestingPage = () => {
   return (
     <Layout>
+      <SEO title="Testing" />
       <main className="page">
         <Gallery />
       </main>
